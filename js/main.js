61f998c990f00941dedb67b92a3dec1beac29e4f
Cristian Gutierrez <yayobyte@gmail.com>
2017-05-26 13:26:02 -0500

    Program select

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function () {
+    changeAction ();
+    });
 
+
+function changeAction () {
+    var page = $('#select1 option:selected').val();
+    $('#form').attr('action','lvl'+page+'.html');
+}
