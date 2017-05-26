6d050aef2555a4753c06fae21d3411a558258e58
Cristian Gutierrez <yayobyte@gmail.com>
2017-05-26 13:36:24 -0500

    Added footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     changeAction ();
     });
 
-
 function changeAction () {
     var page = $('#select1 option:selected').val();
     $('#form').attr('action','lvl'+page+'.html');
